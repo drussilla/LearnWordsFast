8a767f29be076e02a0e9d312cc328306e80ef499
Ivan Derevyanko <drussilla7@gmail.com>
2015-08-13 23:16:28 +0200

    Test project added

---
+++ test/LearnWordsFast.Test/Controllers/HomeControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using LearnWordsFast.Controllers;
+using LearnWordsFast.Repositories;
+using Moq;
+
+namespace LearnWordsFast.Test.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void Index_AllWordsReturned()
+        {
+            var repository = new Mock<IWordRepository>();
+            var target = new HomeController(repository.Object);
+
+            target.Index();
+            
+            repository.Verify(x => x.GetAll(), Times.Once);
+        }
+    }
+}
