3a750198c95b81999a0080a9f3437e7ff23c61f7
tyleronrails <andrijstashuk@gmail.com>
2015-09-17 09:46:09 +0300

    react-bootstrap layout

--- src/LearnWordsFast/Client/js/components/App.jsx
+++ src/LearnWordsFast/Client/js/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, RouteHandler, Link } from 'react-router';
+import {Grid, Row, Col, Navbar, Nav, NavItem} from 'react-bootstrap';
 
 export default class App extends React.Component {
     constructor() {
@@ -8,16 +9,28 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <div className="container">
-                <nav className="navbar navbar-default">
-                    <div className="navbar-header">
-                        <Link to='home'>Home</Link>
-                        <Link to='create'>Create</Link>
-                        <Link to='login'>Login</Link>
-                    </div>
-                </nav>
-                <RouteHandler />
-            </div>
+            <Grid>
+                <Row>
+                    <Col lg={12} md={12}>
+                        <Navbar brand="Learn Words Fast">
+                            <Nav>
+                                <NavItem eventKey={1} href="#/login">Login</NavItem>
+                                <NavItem eventKey={2} href="#/create">Create</NavItem>
+                                <NavItem eventKey={3} href="#/home">Home</NavItem>
+                            </Nav>
+                        </Navbar>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col lg={3} md={3}>
+                    </Col>
+                    <Col lg={6} md={6}>
+                        <RouteHandler />
+                    </Col>
+                    <Col lg={3} md={3}>
+                    </Col>
+                </Row>
+            </Grid>
         );
     }
 }
